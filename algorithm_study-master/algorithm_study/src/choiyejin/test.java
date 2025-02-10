febd885a5aa3efb30ca4f7c3aa9bbb772789f9d0
KoohaKim <toto170@naver.com>
2025-02-11 00:31:03 +0900

    feat: 02/11 solved

--- algorithm_study-master/algorithm_study/src/choiyejin/test.java
+++
@@ -1,7 +0,0 @@
-package choiyejin;
-
-public class test {
-	public static void main(String[] args) {
-		System.out.println("test");
-	}
-}
--- algorithm_study-master/algorithm_study/src/kanghwiwon/test.java
+++
@@ -1,7 +0,0 @@
-package kanghwiwon;
-
-public class test {
-	public static void main(String[] args) {
-		System.out.println("test");
-	}
-}
