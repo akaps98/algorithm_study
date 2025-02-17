858632d3c744758b12ffecfcaf1d4ead6fbfe0c2
Tony Kang <lgtwins0302@gmail.com>
2025-02-17 16:47:58 +0900

    feat: 2/17 solved

---
+++ algorithm_study/src/kangjunsik/programmers/Programmers12943.java
@@ -0,0 +1,33 @@
+package kangjunsik.programmers;
+
+public class Programmers12943 {
+    public static void main(String[] args) {
+        System.out.println(solution(626331));
+    }
+
+    public static int solution(int num) {
+        if(num == 1) {
+            return 0;
+        }
+
+        int answer = 0;
+
+        long tempNum = Long.parseLong(String.valueOf(num)); // Integer는 범위 초과, long으로 변환
+
+        while(tempNum != 1) { // num이 1이 될때까지 돌린다
+            if(tempNum % 2 == 0) {
+                tempNum /= 2;
+            } else {
+                tempNum = tempNum * 3 + 1;
+            }
+
+            answer++; // 연산 반복 값
+
+            if(answer > 500) { // 500 이상이면 끝
+                return -1;
+            }
+        }
+
+        return answer; // 정상 결과시 answer 리턴
+    }
+}
