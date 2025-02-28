b43f48afe49ca3022a0791202e70b8c11d6f2cb7
Tony Kang <lgtwins0302@gmail.com>
2025-02-28 15:29:30 +0900

    feat: 2/28 solved

---
+++ algorithm_study/src/kangjunsik/programmers/Programmers42748.java
@@ -0,0 +1,52 @@
+package kangjunsik.programmers;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42748 
+ * k번째수
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Programmers42748 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i = 0; i < commands.length; i++) {
+            int startIdx = commands[i][0]; // 각 array 자를 시작 인덱스
+            int endIdx = commands[i][1]; // 각 array 자를 끝 인덱스
+            int answerIdx = commands[i][2]; // 숫자를 구할 지정 인덱스
+            
+            int[] tempArr = new int[array.length]; // 매 반복마다 초기화되어야함
+            
+            System.arraycopy(array, 0, tempArr, 0, array.length);
+            
+            ArrayList<Integer> answerArr = new ArrayList<>();
+            
+            boolean isContain = false; // true일 시 answerArr에 넣기
+            
+            if(startIdx == endIdx) { // idx가 같을 시엔 내 알고리즘에서는 숫자가 들어갈 수가 없다. 예외처리부터
+                answerArr.add(tempArr[startIdx - 1]);
+            } else { // 같지 않으면 최소 두개 이상의 숫자가 들어감
+                for(int j = 0; j < tempArr.length; j++) {
+                    if(j == startIdx - 1) { // startIdx - 1이랑 같으면 isContain true로 변경
+                        isContain = true;
+                    } else if (j == endIdx) { // endIdx랑 같으면
+                        isContain = false; // false로 변경 후 for-loop 종료; 이 이상 숫자를 넣을 필요가 없다
+                        break;
+                    }
+                    
+                    if(isContain) { // isContain이 true일동안 모든 숫자를 삽입
+                        answerArr.add(tempArr[j]);
+                    }
+                }   
+            }
+            
+            Collections.sort(answerArr); // 정렬 후
+            
+            answer[i] = answerArr.get(answerIdx - 1); // 정답 담기
+        }
+        
+        return answer;
+    }
+}
